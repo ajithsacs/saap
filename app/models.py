507c9ea0a0a751506b03ca38135b4c5d00a1ffd7
ajiths <ajiths@agilecyber.com>
2024-04-26 11:22:39 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -22,8 +22,12 @@ class Subject(Base):
 
     subject_id = Column(Integer, primary_key=True, index=True, nullable=False)
     subject_name = Column(String(255), index=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
-    updated_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    created_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    updated_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
     deleted = Column(SmallInteger, nullable=False, server_default=text("0"))
     record_status = Column(SmallInteger, nullable=False, server_default=text("1"))
     created_by_id = Column(Integer, nullable=False)
@@ -37,8 +41,12 @@ class Years(Base):
     year_number = Column(Integer, nullable=False)
     assessment_specification = Column(String(255), nullable=False)
     subject_id = Column(Integer, ForeignKey("subject.subject_id"), nullable=False)
-    created_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
-    updated_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    created_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    updated_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
     deleted = Column(SmallInteger, nullable=False, server_default=text("0"))
     record_status = Column(SmallInteger, nullable=False, server_default=text("1"))
     created_by_id = Column(Integer, nullable=False)
@@ -52,8 +60,12 @@ class Question(Base):
     subject_id = Column(Integer, ForeignKey("subject.subject_id"), nullable=False)
     year_id = Column(Integer, ForeignKey("subject_years.year_id"), nullable=False)
     question_text = Column(Text, nullable=False)
-    created_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
-    updated_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    created_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    updated_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
     deleted = Column(SmallInteger, nullable=False, server_default=text("0"))
     record_status = Column(SmallInteger, nullable=False, server_default=text("1"))
     created_by_id = Column(Integer, nullable=False)
@@ -64,12 +76,18 @@ class Options(Base):
     __tablename__ = "options_table"
 
     option_id = Column(Integer, primary_key=True, index=True, nullable=False)
-    question_id = Column(Integer, ForeignKey("questions_table.question_id"), nullable=False)
+    question_id = Column(
+        Integer, ForeignKey("questions_table.question_id"), nullable=False
+    )
     option_value = Column(Text, nullable=False)
     feedback = Column(Text, nullable=False)
     is_correct = Column(SmallInteger, nullable=False)
-    created_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
-    updated_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    created_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    updated_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
     deleted = Column(SmallInteger, nullable=False, server_default=text("0"))
     record_status = Column(SmallInteger, nullable=False, server_default=text("1"))
     created_by_id = Column(Integer, nullable=False)
@@ -81,20 +99,23 @@ class QuestionType(Base):
 
     question_type_id = Column(Integer, primary_key=True, index=True, nullable=False)
     name = Column(Text, nullable=False)
-    created_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
-    updated_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    created_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    updated_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
     deleted = Column(SmallInteger, nullable=False, server_default=text("0"))
     record_status = Column(SmallInteger, nullable=False, server_default=text("1"))
     created_by_id = Column(Integer, nullable=False)
     updated_by_id = Column(Integer)
 
 
-
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True, nullable=False)
-    name = Column(String, nullable=False)
+    name = Column(String(100), nullable=False)
     google_id = Column(String(255), nullable=False, unique=True)
     email = Column(String(255), nullable=False, unique=True)
     deleted = Column(SmallInteger, nullable=False, server_default=text("0"))
